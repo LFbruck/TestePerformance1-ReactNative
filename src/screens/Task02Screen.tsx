99421f599b4f8fab24e8c84ae0f32271d431264c
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-02-02 16:01:44 -0300

    Tarefa 2: Finalizada

--- src/screens/Task02Screen.tsx
+++ src/screens/Task02Screen.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, Alert} from 'react-native';
 
-export default function TaskXXScreen() {
+export default function Task02Screen() {
+
+    const [usuario, setUsuario] = React.useState("");
+    const [senha, setSenha] = React.useState("");
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Tarefa XX - Em desenvolvimento</Text>
+            <Text style={styles.title}>Usuario:</Text>
+
+            <TextInput style={styles.input} placeholder={"Digite seu Usuario"}  value={usuario} onChangeText={setUsuario}  />
+
+            <Text style={styles.title}>Senha:</Text>
+            <TextInput style={styles.input} placeholder={"Digite sua Senha"} value={senha} onChangeText={setSenha}  />
+            <Button title="Entrar" onPress={() => {
+
+                console.log("Botão clicado!");
+                console.log("Usuario:", usuario);
+                console.log("Senha:", senha);
+                if (usuario === "" || senha === "") {
+                    Alert.alert("Invalido! Usuario ou Senha não preenchido!");
+                } else{
+                    Alert.alert("Sucesso!", "Login Efetuado")
+
+            }}}/>
+
         </View>
     );
 }
@@ -16,10 +37,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         padding: 20,
+        gap: 15,
     },
     title: {
         color: 'white',
-        fontSize: 20,
-        textAlign: 'center',
+        fontSize: 40,
+
     },
+    input:{
+        borderColor: "white",
+        backgroundColor: "white",
+        fontSize:20,
+    }
 });
