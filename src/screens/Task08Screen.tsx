dae9e430b9518f94db31fbc8e468f39ebb6775e2
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-02-02 21:12:35 -0300

    Tarefa 8: concluida

--- src/screens/Task08Screen.tsx
+++ src/screens/Task08Screen.tsx
@@ -1,25 +1,57 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
 
-export default function TaskXXScreen() {
+export default function Task08Screen() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Tarefa XX - Em desenvolvimento</Text>
+            <TouchableOpacity style={styles.botao1} onPress={()=> Alert.alert("Voce clicou", "No botão 1!")}>
+                <Text style={styles.title}>Botão 1</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.botao2} onPress={()=> Alert.alert("Botão 2", "Clicado!")}>
+                <Text style={styles.title}>Botão 2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.botao3} onPress={()=> Alert.alert("Opaaa Voce clicou!", "No botão 3!")}>
+                <Text style={styles.title}>Botão 3</Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'darkslategray',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-    },
-    title: {
-        color: 'white',
-        fontSize: 20,
-        textAlign: 'center',
-    },
-});
+        container: {
+            flex: 1,
+            backgroundColor: 'darkslategray',
+            gap:40,
+            padding: 40,
+        },
+        title: {
+            color: 'white',
+            fontSize: 20,
+            textAlign: 'center',
+        },
+        botao1: {
+            width: '100%',
+            height:100,
+            backgroundColor: "brown",
+            borderRadius: 5,
+
+            justifyContent: 'center',
+        },
+        botao3:{
+            width:200,
+            height:200,
+            backgroundColor: "purple",
+            borderRadius: 10,
+            justifyContent: 'center'
+
+        },
+        botao2:{
+            width:100,
+            height:300,
+            backgroundColor: "yellow",
+            borderRadius: 1,
+            justifyContent: 'center'
+        }
+    }
+)
