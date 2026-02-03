3cb584e9994b1a194789ba1b5631005737ad2f58
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-02-02 21:37:51 -0300

    Tarefaa 10: Feita

--- src/screens/Task10Screen.tsx
+++ src/screens/Task10Screen.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
-export default function TaskXXScreen() {
+export default function Task10Screen() {
+    // Array de itens da lista de compras
+    const listaCompras = [
+        { id: "1", nome: "Arroz" },
+        { id: "2", nome: "Feijão" },
+        { id: "3", nome: "Macarrão" },
+        { id: "4", nome: "Óleo" },
+        { id: "5", nome: "Açúcar" },
+        { id: "6", nome: "Café" },
+        { id: "7", nome: "Leite" },
+        { id: "8", nome: "Pão" },
+        { id: "9", nome: "Manteiga" },
+        { id: "10", nome: "Ovos" },
+    ];
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Tarefa XX - Em desenvolvimento</Text>
+            <Text style={styles.titulo}>Lista de com flatlist</Text>
+
+            <FlatList
+                data={listaCompras}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <View style={styles.itemContainer}>
+                        <Text style={styles.itemTexto}>{item.nome}</Text>
+                    </View>
+                )}
+            />
         </View>
     );
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'darkslategray',
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: "darkslategray",
         padding: 20,
     },
-    title: {
-        color: 'white',
-        fontSize: 20,
-        textAlign: 'center',
+    titulo: {
+        color: "white",
+        fontSize: 28,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginBottom: 20,
+        marginTop: 20,
+    },
+    itemContainer: {
+        backgroundColor: "#ffffff20",
+        padding: 15,
+        marginVertical: 8,
+        borderLeftColor: "white",
+    },
+    itemTexto: {
+        color: "white",
+        fontSize: 18,
     },
 });
