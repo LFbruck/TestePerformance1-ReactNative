61a4256a7f9d6e640dbf7cc6c3566475964115aa
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-02-08 19:36:32 -0300

    Tarefa 13 pronta

--- src/screens/Task13Screen.tsx
+++ src/screens/Task13Screen.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
-export default function TaskXXScreen() {
+export default function Task13Screen() {
+
+    const [contador, setContador] = useState(0);
+    const diminuir = () => {
+        if (contador > 0) {
+            setContador(contador - 1);
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Tarefa XX - Em desenvolvimento</Text>
+            <Button  color="green" title="Clique aqui para aumentar" onPress={() => setContador( contador + 1)} />
+            <Button  color="red" title="Clique aqui pra diminuir" onPress={diminuir}></Button>
+
+            <Text style={styles.texto}>Numero de cliques: {contador}</Text>
+
         </View>
     );
 }
@@ -16,10 +28,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         padding: 20,
+        gap:20,
     },
     title: {
         color: 'white',
         fontSize: 20,
         textAlign: 'center',
     },
+    texto: {
+        backgroundColor: "orange",
+        fontSize: 40,
+    },
+
 });
