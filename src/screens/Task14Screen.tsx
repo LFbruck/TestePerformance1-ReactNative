5ca9da27485263d7d77f43502701c5aebfde83ee
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-02-08 19:49:15 -0300

    Tarefa 14 finalizada

--- src/screens/Task14Screen.tsx
+++ src/screens/Task14Screen.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import {Pressable} from 'react-native';
 
-export default function TaskXXScreen() {
+export default function Task14Screen() {
+
+    const cores = ["red", "blue", "green", "yellow", "purple", "pink", "brown", "orange"];
+
+    const [fundo, setFundo] = useState("grey");
+    const mudarCor = ()=>{
+        const corAleatoria = cores[Math.floor(Math.random() * cores.length)];
+        setFundo(corAleatoria);
+    }
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tarefa XX - Em desenvolvimento</Text>
+        <View style={[styles.container,{backgroundColor:fundo}]}>
+            <Pressable onPress={mudarCor} style={styles.botao} >
+                <Text style={styles.texto}>Clique para mudar a cor de fundo</Text>
+            </Pressable>
         </View>
     );
 }
@@ -12,14 +23,20 @@ export default function TaskXXScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'darkslategray',
         justifyContent: 'center',
         alignItems: 'center',
         padding: 20,
     },
-    title: {
-        color: 'white',
-        fontSize: 20,
-        textAlign: 'center',
+    botao: {
+        backgroundColor: "black",
+        borderRadius: 10,
+        height: 100,
+        justifyContent: 'center',
+
     },
+    texto: {
+        color: "white",
+        fontSize: 40,
+    }
+
 });
